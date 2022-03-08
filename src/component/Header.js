e778f8a44c2d7705cb70de241e89c3671f8ec399
Hadi Ahmed <hadiahmed098@gmail.com>
2022-03-08 13:06:43 -0500

    Small header image fix

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
         const header =
         <div className="header">
             <h1>PokeDex Pokemon Viewer - Beginner</h1>
-            <a href="https://purdueieee.org/software"><img src="/pokedex/ss-logo.svg" width="10%" height="auto" alt="Software Saturdays Logo"/></a>
+            <a href="https://purdueieee.org/software"><img src="/pokedex-beginner/ss-logo.svg" width="10%" height="auto" alt="Software Saturdays Logo"/></a>
             <h3>Developed during Software Saturdays Spring 2021</h3>
             <h3>For more information, visit <a href="https://purdueieee.org/software">our website</a>.</h3>
             <hr />
